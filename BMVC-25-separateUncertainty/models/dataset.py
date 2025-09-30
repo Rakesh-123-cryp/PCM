75f5eeadc25e45fc52f185cb2842b0c043de35cf
Rakesh-123-cryp <roketraj03@gmail.com>
2025-09-30 15:43:35 +0200

    Small bug for dataset loading

--- BMVC-25-separateUncertainty/models/dataset.py
+++ BMVC-25-separateUncertainty/models/dataset.py
@@ -91,7 +91,6 @@ class Dataset:
         if self.use_init:
             camera_dict = np.load(os.path.join(self.data_dir, self.render_cameras_name))
             self.camera_dict = camera_dict
-            print(if "world_mat_107" in camera_dict.keys())
             if self.data_type == 'DTU':
                 try:
                     self.world_mats_np = [camera_dict['world_mat_{}'.format(int(os.path.basename(idx)[:-4]))].astype(np.float32) for idx in self.images_lis]
